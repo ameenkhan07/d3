a5c5094815d27edfa68a0ed1b4f8e52b3bcc3cae
Ameen <ameenkhan07@gmail.com>
2016-05-03 10:31:54 +0530

    commit 4

--- d3.js
+++ d3.js
@@ -5,19 +5,6 @@
   }
   if (d3_document) {
     try {
-      d3_array(d3_document.documentElement.childNodes)[0].nodeType;
-    } catch (e) {
-      d3_array = function(list) {
-        var i = list.length, array = new Array(i);
-        while (i--) array[i] = list[i];
-        return array;
-      };
-    }
-  }
-  if (!Date.now) Date.now = function() {
-    return +new Date();
-  };
-  if (d3_document) {
     try {
       d3_document.createElement("DIV").style.setProperty("opacity", 0, "");
     } catch (error) {
